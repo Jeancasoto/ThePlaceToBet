897fba16867556cc81f81b42ec2dd0e3a61e9b4c
Jean Soto <jeancasoto@unitec.edu>
2018-11-18 15:43:52 -0600

    intento arreglar GUI 3.0

--- truco.py
+++ truco.py
@@ -4,7 +4,7 @@ from PyQt4 import QtCore, QtGui, uic
 class Ui_MainWindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
-        uic.loadUi("mainGui.ui", self)
+        uic.loadUi("ThePlaceToBet.ui", self)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
