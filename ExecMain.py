6e8c4a58bdc5598b0689f99538279944993584de
Jean Soto <jeancasoto@unitec.edu>
2018-11-20 06:47:50 -0600

    prueba conectando dialog 1.0

--- ExecMain.py
+++ ExecMain.py
@@ -5,16 +5,23 @@ class Ui_MainWindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         uic.loadUi("ThePlaceToBet.ui", self)
-        self.actionPrueba.triggered.connect(self.hola)
-        self.actionPrueba2.triggered.connect(self.adios)
+        #Llamar a la ventana que muestra un login para el admin
+        self.menu_admin.clicked.connect(self.login)
+    #Metodo que manda a llamar el dialog
+    def login(self):
+        self.nd = NewDialog(self)
+        self.nd.show()
+        print 'deberia'
+    
+class Plugin:
+    def __init__(self, iface):
+        self.dlg.button.connect(self.open_new_dialog)
 
 
-    def hola(self):
-        print 'Hola'
-
-    def adios(self):
-        print 'adios'
-
+#Creacion del Dialog admin     
+class Login_as_admin(QtGui.QWidget):
+    def __init__(self, parent):
+        super(NewDialog, self).__init__(parent) 
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
