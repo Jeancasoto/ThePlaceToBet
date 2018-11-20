e8344d01a7377b2acbb78c8ec6651a8b25ce7943
Jean Soto <jeancasoto@unitec.edu>
2018-11-20 06:54:57 -0600

    prueba conectando dialog 5.0

--- ExecMain.py
+++ ExecMain.py
@@ -6,13 +6,13 @@ class Ui_MainWindow(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self)
         uic.loadUi("ThePlaceToBet.ui", self)
         #Llamar a la ventana que muestra un login para el admin
-        self.menu_exit.triggered.connect(self.exit)
+        self.menu_exit.triggered.connect(self.menu_salir)
     #Metodo que manda a llamar el dialog
     def login(self):
         #self.nd = NewDialog(self)
         #self.nd.show()
         #print 'deberia'
-    def exit(self):
+    def menu_salir(self):
         QCoreApplication.exit(0)
         print 'deberia'
     
