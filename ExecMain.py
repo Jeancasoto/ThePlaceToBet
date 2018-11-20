7f5fca0bd0bd1fefd269aae928607ac1c8d29f67
Jean Soto <jeancasoto@unitec.edu>
2018-11-20 09:42:22 -0600

    prueba dialog 8.0

--- ExecMain.py
+++ ExecMain.py
@@ -14,11 +14,11 @@ class Ui_MainWindow(QtGui.QMainWindow):
         #self.nd.show()
         print 'alv perro'
     def menu_salir(self):
+        app = QtGui.QApplication(sys.argv)
         Dialog = QtGui.QDialog()
         ui = Ui_Dialog()
         ui.setupUi(Dialog)
         Dialog.show()
-        #sys.exit(app.exec_())
         #sys.exit()
         #print 'Salir del sistema exitoso'
     
