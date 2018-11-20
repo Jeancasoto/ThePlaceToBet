ee7aa18d9ae390449c78dfbd92fcca0d9632c0c4
Jean Soto <jeancasoto@unitec.edu>
2018-11-20 15:44:17 -0600

    create database script

---
+++ createDB.py
@@ -0,0 +1,17 @@
+from couchdb.mapping import Document, TextField, IntegerField, Mapping
+from couchdb.mapping import DictField, ViewField, BooleanField, ListField
+from couchdb import Server
+import couchdb
+
+#Hace la coneccion local automaticamente 
+server = Server()
+
+#Crear una database
+db = server.create('theplacetobet')
+
+print 'Programa finalizado con exito'
+
+
+msg = QMessageBox()
+msg.setIcon(QMessageBox.Information)
+msg.setText("Base de datos creada con exito"
