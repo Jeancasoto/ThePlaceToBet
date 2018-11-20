e024aa269f419adf74cf9843cc301b9fb337a808
Daniel Suazo <suazodaniel99@gmail.com>
2018-11-20 13:20:06 -0600

    Agrega a una base de datos local quinela

--- ExecMain.py
+++ ExecMain.py
@@ -1,8 +1,13 @@
 import sys
+from couchdb.mapping import Document, TextField, IntegerField, Mapping
+from couchdb.mapping import DictField, ViewField, BooleanField, ListField
+from couchdb import Server
+import couchdb
 from PyQt4 import QtCore, QtGui, uic
 from crearPersonaGui import Ui_Dialog
 
 class Ui_MainWindow(QtGui.QMainWindow):
+
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         uic.loadUi("ThePlaceToBet.ui", self)
@@ -10,16 +15,16 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.menu_exit.triggered.connect(self.menu_salir)
         #Llamar metodo para exec dialog crear persona
         self.menu_crear_persona.triggered.connect(self.exec_crear_persona)
-        #self.boton_confirmar.clicked.connect(self.recuperarPersona)
+        
     #Metodo que manda a llamar el dialog
     def login(self):
         #self.nd = NewDialog(self)
         #self.nd.show()
-        print 'alv perro'
+        print ('alv perro')
     #Metodo al accionar el boton de exit
     def menu_salir(self):
         sys.exit()
-        print 'Salir del sistema exitoso'
+        print ('Salir del sistema exitoso')
     #Metodo para exec gui crear persona 
     def exec_crear_persona(self):
         Dialog = QtGui.QDialog()
@@ -27,8 +32,9 @@ class Ui_MainWindow(QtGui.QMainWindow):
         ui.setupUi(Dialog)
         Dialog.show()
         Dialog.exec_()
-        print 'Debe exec el dialog de crear persona'
+        print ('Debe exec el dialog de crear persona')
 
+    """
     def recuperarPersona(self):
         fechaN = self.fecha_nacimiento.date()
         fechaN = fechaN.toPyDate()
@@ -44,7 +50,25 @@ class Ui_MainWindow(QtGui.QMainWindow):
         print(rol)
         print(peso)
 
+        serverCDB = Server()
+        db = serverCDB['quinelas']
+
+        if ( db[identidad] is not None ):
+            docPersona = {
+                '_id': identidad,
+                'content': {
+                    'nombre': pNombre,
+                    'apellido': pApellido,
+                    'fechaN': fechaN,
+                    'rol': rol,
+                    'peso': peso
+                }
+            }
+            db.save(docPersona)
+        else:
+            print("Ya existe una persona con ese ID")
         #-----faltaria hacer el insert aqui-----
+        """
 
     def prueba(self):
         print("byeee")    
