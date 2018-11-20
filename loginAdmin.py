d23b2e22d949b8230b62aa88691dd7973ec90f4a
Jean Soto <jeancasoto@unitec.edu>
2018-11-20 14:57:51 -0600

    login as admin 22.0

--- loginAdmin.py
+++ loginAdmin.py
@@ -63,7 +63,18 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
-
+        #Boton que desbloquea las opciones de admin
+        self.boton_login.clicked.connect(self.button_login)
+    
+    #Metodo que desbloquea las opciones del admin
+    def button_login(self):
+        if self.username == 'admin':
+            print 'Username coincide'
+            if self.password == 'admin':
+                print 'password coincide'
+                self.menu_crear_persona.setEnabled
+                self.menu_manage_team.setEnabled
+                print 'deberia haberlos desbloqueado'
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
         self.boton_login.setText(_translate("Dialog", "Log In", None))
